20284536a80d49ed0d0337029a9127b1937601a3
rar-rahul <rahulraut430@gmail.com>
2025-02-06 12:05:47 -0500

    route changes

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,10 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../Controllers/userController')
 
-
-
+//route handlers
 router.post('/register',userController.register)
-router.get('/login',userController.login)
+router.post('/login',userController.login)
 
 
-
+//export router
 module.exports = router
